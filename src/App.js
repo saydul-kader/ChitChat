c52ad2d30f72d263b0a15c3fe78ae4b62256a076
saydul-kader <saydul.kader@gmail.com>
2021-05-03 23:28:20 +0600

    ChitChat app updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import img from './chit.png';
 import './App.css';
-
+const projectID = 'c051def3-4bf3-401a-9b4a-38297a0db81d';
 function App() {
+  const handleLogOut= ()=> {
+      localStorage.clear();
+      window.location.reload();
+  }
+  if (!localStorage.getItem('username')) return <LoginForm />;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+       <div class="button-container">
+         <img src={img} alt="logo"/>
+        <button class="btn" onClick={handleLogOut}><span className="btn-word">Log out</span></button>
+       </div>
+    <ChatEngine
+          height="93vh"
+          projectID={projectID}
+          userName={localStorage.getItem('username')}
+          userSecret={localStorage.getItem('password')}
+          renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+          onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+    
+   </div> 
   );
 }
 
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import axios from 'axios';
+import img from '../chit2.png';
+
+const projectID = 'c051def3-4bf3-401a-9b4a-38297a0db81d';
+
+const Modal = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+
+    try {
+      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+
+      window.location.reload();
+      setError('');
+    } catch (err) {
+      setError('Oops, incorrect credentials.');
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
+        <img src={img} className="title" alt="logoo"/>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+          <div align="center">
+            <button type="submit" className="button">
+              <span>Start chatting </span>
+            </button>
+          </div>
+        </form>
+        <h1>{error}</h1>
+      </div>
+    </div>
+
+  );
+};
+
+export default Modal;
